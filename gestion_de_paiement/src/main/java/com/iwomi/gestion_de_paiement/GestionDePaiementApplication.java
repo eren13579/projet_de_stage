16665f300b2c89b966f09cfb219bb2cf5cf033a9
eren13579 <168607280+eren13579@users.noreply.github.com>
2024-07-24 16:05:42 +0100

    main

---
+++ gestion_de_paiement/src/main/java/com/iwomi/gestion_de_paiement/GestionDePaiementApplication.java
@@ -0,0 +1,13 @@
+package com.iwomi.gestion_de_paiement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDePaiementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDePaiementApplication.class, args);
+    }
+
+}
---
+++ gestion_de_paiement/src/test/java/com/iwomi/gestion_de_paiement/GestionDePaiementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwomi.gestion_de_paiement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDePaiementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
